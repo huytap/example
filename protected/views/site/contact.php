37c74017d5db8b2c8acd05bb2b597a8406389a50
nghuytap@gmail.com <nghuytap@gmail.com>
2014-09-23 15:58:27 +0700

    update

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -3,38 +3,17 @@
 				<div class="content">
 					<article class="content-left">
 						<h2>Liên hệ</h2>				
-						<div>							
+						<div>
 							<p>Cty TNHH MTV Môi Trường Đô Thị TP HCM xin chân thành cám ơn sự quan tâm, ủng hộ của quý khách đến các hoạt động của chúng tôi.</p>
+						<?php
+						foreach ($model as $key => $value):?>						
 							<p>
-								<strong>Văn phòng Xí nghiệp Vận chuyển số 1</strong><br>
-								12 Quang Trung - Phường 11 - Quận Gò Vấp<br>
-								Điện thoại: (08) 39966834 – 39968927 – 38958675<br>
-								Fax: (08) 39968926
+								<strong><?php echo $value['title']?></strong><br>
+								<?php echo $value['address']?><br>
+								Điện thoại: <?php echo $value['tel']?><br>
+								Fax: <?php echo $value['fax']?>
 							</p>
-							<p>
-								<strong>Văn phòng Xí nghiệp Vận chuyển số 2</strong><br>
-								Số 01 Tống Văn Trân - Phường 5 - Quận 11<br>
-								Điện thoại: (08) 38653614 - 38619857 - 38618818<br>
-								Fax: (08) 38618818
-							</p>
-							<p>
-								<strong>Văn phòng Xí nghiệp Vận chuyển số 3</strong><br>
-								150 Lê Đại Hành - Phường 7 - Quận 11<br>
-								 Điện thoại: (08) 38557783 - 38555664 <br>
-								 Fax : (08) 38557783
-							</p>
-							<p>
-								<strong>Văn phòng Xí nghiệp Dịch vụ Môi trường</strong><br>
-								Địa chỉ: 18 Kinh Dương Vương, Phường 13, Quận 06<br>
-								Điện thoại: (08) 38756115<br>
-								Fax : (08) 38754892
-							</p>
-							<p>
-								<strong>Xí nghiệp Xử lý chất thải</strong><br>
-									Địa chỉ: 377 Lê Văn Khương, Phường Hiệp Thành, Quận 12<br>
-									Điện thoại: (08) 37176016<br>
-									Fax : (08) 37175812
-							</p>
+						<?php endforeach;?>
 						</div>
 					</article>
 					<article class="content-right">
