8b51e5542c759aad8cb6712bc60735c0556cb9d6
nghuytap@gmail.com <nghuytap@gmail.com>
2014-10-13 21:27:46 +0700

    update favicon

--- protected/components/views/css.php
+++ protected/components/views/css.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="favicon.ico">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel='stylesheet' href='<?php echo Yii::app()->theme->baseUrl?>/frontend/css/bootstrap.css' />
 <link rel='stylesheet' href='<?php echo Yii::app()->theme->baseUrl?>/frontend/css/flexslider.css' />
